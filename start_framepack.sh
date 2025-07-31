748bac786131cdb122c7694b15be4269d6e2b8ce
danny-moldovan <daniel.mda@gmail.com>
2025-07-31 19:44:21 +0000

    Changed output folder for FramePack

--- start_framepack.sh
+++ start_framepack.sh
@@ -1,16 +1,5 @@
-git clone https://github.com/lllyasviel/FramePack.git
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126 --upgrade
 pip install -r FramePack/requirements.txt --upgrade
-
-git clone https://github.com/danny-moldovan/image_and_video_generation_on_RunPod.git
-cp -r image_and_video_generation_on_RunPod/. .
-pip install -r FramePack_serving/requirements.txt
-mv demo_gradio.py FramePack/
-mv download_models_for_FramePack.py FramePack/
-chmod +x start_ollama.sh
-chmod +x start_framepack.sh
-chmod +x monitor_pod_and_terminate.py
-pip install runpod python-dotenv asyncio
 
 cp /workspace/clip_l.safetensors ComfyUI/models/text_encoders/
 mkdir -p ComfyUI/models/text_encoders/t5
